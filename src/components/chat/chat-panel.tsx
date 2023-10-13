9665aad6eb5cb591e0dc17bc61df9dc4d3699dda
Flora Liu <flofloliu@gmail.com>
2023-10-13 10:15:14 -0700

    fix: chat panel spacing

--- src/components/chat/chat-panel.tsx
+++ src/components/chat/chat-panel.tsx
@@ -45,9 +45,9 @@ export function ChatPanel({
     <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
       {/* <ButtonScrollToBottom /> */}
       <div className="mx-auto sm:max-w-2xl sm:px-4">
-        <div className="border-t bg-background px-4 shadow-lg sm:rounded-t-xl sm:border">
+        <div className="border-t bg-background px-4 sm:py-4 shadow-lg sm:rounded-t-xl sm:border">
           {suggestions && suggestions?.length > 0 && (
-            <div className="flex flex-wrap gap-2 mt-3 sm:mb-3">
+            <div className="flex flex-wrap gap-2 mt-3 sm:mt-0 sm:mb-3">
               {suggestions?.map((suggestion, index) => (
                 <button
                   key={index}
