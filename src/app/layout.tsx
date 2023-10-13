3c690f72e490987462a47cdc2ab8cb54f9d087f2
Flora Liu <flofloliu@gmail.com>
2023-10-12 18:51:12 -0700

    update: add auth provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Nav } from "@/components/common/nav";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import AuthProvider from "@/components/common/auth-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,30 +14,32 @@ export const metadata: Metadata = {
   title: "Casa Kind",
   description: "Miniapp for self-care and lifelong blossoming.",
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: light)", color: "light" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const accessToken = session?.access_token || null;
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
       <body className={inter.className}>
-        <Providers
-          attribute="class"
-          defaultTheme="white"
-          // defaultTheme="system"
-          enableSystem
-        >
+        <Providers attribute="class" defaultTheme="light" enableSystem>
           <div className="flex flex-col min-h-screen relative text-foreground">
             <Nav />
             <main className="flex flex-col flex-1 bg-background h-full">
-              {children}
+              <AuthProvider accessToken={accessToken}>{children}</AuthProvider>
             </main>
           </div>
         </Providers>
