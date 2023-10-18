4d50b98cd32cb9222a355b8f0fa82c54269560a0
Flora Liu <flofloliu@gmail.com>
2023-10-18 13:19:01 -0700

    update: add exit button to new entry page

--- src/components/journal/journal-form.tsx
+++ src/components/journal/journal-form.tsx
@@ -13,7 +13,7 @@ interface FormInputProps {
 function FormInput({ input, setInput }: FormInputProps) {
   const inputRef = React.useRef<HTMLTextAreaElement>(null);
   return (
-    <div className="py-4 md:py-6">
+    <div className="py-4 md:py-6 w-full">
       <Textarea
         ref={inputRef}
         tabIndex={0}
@@ -34,34 +34,29 @@ const prompt = "When do you feel most connected to yourself?";
 export default function JournalForm() {
   const [input, setInput] = useState<string>("");
   return (
-    <div className="min-h-screen w-full flex flex-col justify-start items-center mx-auto sm:max-w-2xl">
-      <div className="py-20 md:py-16 px-5 md:px-0 w-full">
-        <Tabs
-          defaultValue="prompt"
-          className="flex flex-col items-center w-full"
-        >
-          <TabsList className="bg-accent/40">
-            <TabsTrigger value="prompt">Prompt of the day</TabsTrigger>
-            <TabsTrigger value="freeform">Freeform</TabsTrigger>
-          </TabsList>
-          <TabsContent value="prompt" className="w-full">
-            <p className="pt-4 md:pt-8 pb-2 md:pb-3 leading-normal text-sm md:text-base text-muted-foreground">
-              {category}
-            </p>
-            <p className="font-relative tracking-tight text-2xl md:text-3xl">
-              {prompt}
-            </p>
-            <FormInput input={input} setInput={setInput} />
-          </TabsContent>
-          <TabsContent value="freeform" className="w-full">
-            <FormInput input={input} setInput={setInput} />
-          </TabsContent>
-        </Tabs>
-        <Button size="md" className="flex items-center">
-          <IconCheck className="mr-1" />
-          I&apos;m finished
-        </Button>
-      </div>
+    <div className="px-5 md:px-0 w-full">
+      <Tabs defaultValue="prompt" className="flex flex-col items-center w-full">
+        <TabsList className="bg-accent/40">
+          <TabsTrigger value="prompt">Prompt of the day</TabsTrigger>
+          <TabsTrigger value="freeform">Freeform</TabsTrigger>
+        </TabsList>
+        <TabsContent value="prompt" className="w-full">
+          <p className="pt-4 md:pt-8 pb-2 md:pb-3 leading-normal text-sm md:text-base text-muted-foreground">
+            {category}
+          </p>
+          <p className="font-relative tracking-tight text-2xl md:text-3xl">
+            {prompt}
+          </p>
+          <FormInput input={input} setInput={setInput} />
+        </TabsContent>
+        <TabsContent value="freeform" className="w-full">
+          <FormInput input={input} setInput={setInput} />
+        </TabsContent>
+      </Tabs>
+      <Button size="md" className="flex items-center">
+        <IconCheck className="mr-1" />
+        I&apos;m finished
+      </Button>
     </div>
   );
 }
