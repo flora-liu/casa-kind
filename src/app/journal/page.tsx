c758855ddde4289fe83c982d6eab1c14cefbb547
Flora Liu <flofloliu@gmail.com>
2023-10-24 18:23:04 -0700

    fix: conditionally show daily prompt

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import { getDailyGratitudeEntries, getDailyPrompt } from "@/app/actions";
 
 async function Page() {
-  const promptOfTheDay = await getDailyPrompt();
+  const dailyPrompt = await getDailyPrompt();
   const gratitudeData = await getDailyGratitudeEntries();
   return (
     <Layout
@@ -14,23 +14,25 @@ async function Page() {
       subtitle="Cultivate awareness through observing what's on your mind"
       headerNav={<JournalNav />}
     >
-      <Section title="Prompt of the day">
-        <div className="bg-card text-card-foreground rounded-lg p-4 md:p-6 border border-border min-h-[14rem] flex flex-col justify-between gap-y-2.5 md:gap-y-3">
-          <p className="text-sm text-muted-foreground">
-            {promptOfTheDay?.category?.title}
-          </p>
-          <h2 className="text-xl md:text-3xl font-relative tracking-tight">
-            {promptOfTheDay?.prompt?.title}
-          </h2>
-          <div>
-            <Button variant="secondary" size="sm" asChild>
-              <a href="/journal/entry/new">
-                <IconPencil className="mr-1.5 h-3 w-3" /> Answer
-              </a>
-            </Button>
+      {dailyPrompt && (
+        <Section title="Prompt of the day">
+          <div className="bg-card text-card-foreground rounded-lg p-4 md:p-6 border border-border min-h-[14rem] flex flex-col justify-between gap-y-2.5 md:gap-y-3">
+            <p className="text-sm text-muted-foreground">
+              {dailyPrompt?.category?.title}
+            </p>
+            <h2 className="text-xl md:text-3xl font-relative tracking-tight">
+              {dailyPrompt?.prompt?.title}
+            </h2>
+            <div>
+              <Button variant="secondary" size="sm" asChild>
+                <a href="/journal/entry/new">
+                  <IconPencil className="mr-1.5 h-3 w-3" /> Answer
+                </a>
+              </Button>
+            </div>
           </div>
-        </div>
-      </Section>
+        </Section>
+      )}
       <Section title="Daily gratitude">
         <GratitudeForm {...gratitudeData} />
       </Section>
