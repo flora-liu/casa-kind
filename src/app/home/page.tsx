362eb90ea740fd4def633fa3e6b378b4595b8514
Flora Liu <flofloliu@gmail.com>
2023-10-31 23:35:38 -0700

    update: home page styles

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
 import { IconChevronRight } from "@/components/common/icons";
 import { Layout, Section } from "@/components/common/layout";
 import { Link } from "@/components/ui/link";
-import { getRandomPrompts } from "@/app/actions";
+import { BlurImage } from "@/components/ui/blur-image";
 
 const features = [
   {
@@ -25,60 +25,85 @@ const features = [
 ];
 
 export default async function Page() {
-  const prompts = await getRandomPrompts();
   return (
-    <Layout
-      title="Welcome"
-      subtitle="Warm hugs from Casa Kind, we're thrilled you're here."
-    >
-      <Section className="relative flex h-full flex-col items-center justify-center mt-2 md:mt-6">
-        <div className="md:max-w-7xl md:grid md:grid-cols-2 bg-card pt-2 pb-4 md:py-8 rounded-xl border border-border">
-          <div className="flex-1 w-full relative h-full flex-col p-5 md:px-10 md:py-4 dark:border-r md:flex">
-            <div className="font-relative relative z-20 flex items-center text-4xl md:text-6xl lg:text-7xl mb-3">
-              <h1>Self-Care Menu</h1>
+    <>
+      <div className="relative min-h-[30rem] md:min-h-[42rem] px-5 py-12 md:px-16 md:py-24 flex flex-col items-center justify-center">
+        <div className="text-center grid grid-cols-12 md:max-w-7xl w-full z-30">
+          <div className="col-span-12 mt-12">
+            <h1 className="font-relative text-3xl md:text-4xl lg:text-5xl leading-tight md:leading-snug mb-3 md:mb-6">
+              Wherever you are, <br /> take a moment to{" "}
+              <span className="font-cambon italic">ground</span>
+            </h1>
+            <p className="text-lg md:text-xl">Anchor yourself in this moment</p>
+          </div>
+        </div>
+        <div className="absolute w-full h-full">
+          <div className="relative w-full h-full overflow-hidden">
+            <BlurImage
+              alt="Palm leaf"
+              src="/images/steinar-engeland-BfMbxUu0EGE-unsplash.jpg"
+              sizes="100vw"
+              className="absolute w-full h-full object-center z-10"
+              fill
+              aspectRatio="aspect-w-3 aspect-h-4 md:aspect-h-3 md:aspect-w-4"
+            />
+            <div className="z-20 inset-0 absolute w-full h-full bg-orange-100 opacity-50 md:opacity-40" />
+          </div>
+        </div>
+      </div>
+      <Layout className="min-h-0 pb-12">
+        <Section
+          innerStyles="py-2 w-full"
+          className="relative flex h-full flex-col items-center justify-center"
+        >
+          <div className="md:max-w-7xl w-full md:grid md:grid-cols-2">
+            <div className="flex-1 w-full relative h-full flex-col py-5 md:flex">
+              <div className="font-relative relative z-20 flex items-center text-3xl md:text-5xl mb-3">
+                <h1>Self-Care Menu</h1>
+              </div>
+              <div className="relative mt-2">
+                <div className="space-y-2">
+                  <p className="text-lg text-muted-foreground">
+                    Pick one to nourish your being
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="relative mt-2">
-              <div className="space-y-2">
-                <p className="text-lg text-muted-foreground">
-                  Pick one to nourish your being
-                </p>
+            <div className="lg:px-8 md:py-4 relative flex-auto w-full">
+              <div className="relative w-full h-full flex flex-col gap-y-3 py-2 md:gap-y-4">
+                {features.map((feature, index) => (
+                  <div
+                    key={index}
+                    className="relative mx-auto flex w-full flex-col justify-center space-y-4 z-20 rounded-xl bg-[#E3DECB] hover:bg-[#E3DECB]/80 text-secondary-foreground"
+                  >
+                    <Link
+                      className="flex flex-col space-y-2 text-left px-4 py-4 md:py-8 w-full"
+                      href={feature.href}
+                      variant="basic"
+                    >
+                      <div className="flex items-center gap-2.5">
+                        <h2 className="text-lg md:text-xl font-medium tracking-tight">
+                          {feature.name}
+                        </h2>
+                        <IconChevronRight className="h-3 w-3" />
+                      </div>
+                      <div className="flex flex-row flex-nowrap justify-between gap-x-3 items-center text-base">
+                        <p className="text-muted-foreground shrink-0">
+                          {feature.description}
+                        </p>
+                        <div className="h-px w-full flex-grow border-dotted border-b-2 border-b-muted-foreground/60"></div>
+                        <p className="text-muted-foreground shrink-0">
+                          {feature.duration}
+                        </p>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-          <div className="lg:px-8 md:py-4 relative flex-auto w-full">
-            <div className="relative w-full h-full flex flex-col gap-y-3 py-2 md:gap-y-4 px-5 md:py0">
-              {features.map((feature, index) => (
-                <div
-                  key={index}
-                  className="relative mx-auto flex w-full flex-col justify-center space-y-4 z-20 rounded-xl bg-secondary hover:bg-secondary/80 text-secondary-foreground"
-                >
-                  <Link
-                    className="flex flex-col space-y-2 text-left px-4 py-4 md:py-8 w-full"
-                    href={feature.href}
-                    variant="basic"
-                  >
-                    <div className="flex items-center gap-2.5">
-                      <h2 className="text-lg md:text-xl font-medium tracking-tight">
-                        {feature.name}
-                      </h2>
-                      <IconChevronRight className="h-3 w-3" />
-                    </div>
-                    <div className="flex flex-row flex-nowrap justify-between gap-x-3 items-center text-base">
-                      <p className="text-muted-foreground shrink-0">
-                        {feature.description}
-                      </p>
-                      <div className="h-px w-full flex-grow border-dotted border-b-2 border-b-muted-foreground/60"></div>
-                      <p className="text-muted-foreground shrink-0">
-                        {feature.duration}
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </Section>
-    </Layout>
+        </Section>
+      </Layout>
+    </>
   );
 }
